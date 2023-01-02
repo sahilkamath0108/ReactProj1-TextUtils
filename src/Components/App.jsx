97b8d865d15239780c1e510cbc0e1c17b18af6a8
Sahil Kamath <sahilkamath0108@gmail.com>
2023-01-02 18:10:10 +0530

    first commit again

---
+++ src/Components/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Navbar from "./Navbar";
+import TextForm from "./TextForm";
+// import About from "./About";
+import { useState } from "react";
+import Alert from "./Alert";
+
+function App(){
+
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) =>{
+    setAlert({
+      message : message,
+      type : type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    }, 1000)
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor="grey"
+      showAlert("Dark mode has been enabled", "success")
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor="white"
+      showAlert("Light mode has been enabled", "success")
+    }
+  }
+
+  return (
+    <>
+    <Navbar title="TextUtils" aboutText="About TextUtils"  mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <TextForm showAlert={showAlert} heading="Enter the Text to analyze" mode={mode} />
+    {/* <About /> */}
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/TextForm.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+  
+  const handleUC = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "success")
+  };
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox")
+    text.select()
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value)
+    props.showAlert("Copied to clipboard", "success")
+  }
+
+  const handleLC = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase", "success")
+  };
+
+  const clearText = () => {
+    setText("")
+    props.showAlert("Cleared text", "success")
+  }
+
+  const handleCapitalize = () => {
+    let newText = text.split(" ").map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(" ");
+    setText(newText);
+    props.showAlert("Capitalized the text", "success")
+  }
+
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+    props.showAlert("Message has been spoken", "success")
+  }
+
+  const handleToggleCaseClick = () => {
+    let words = text.split(" ");
+    let newText = words
+      .map((word) => {
+        let newWord = "";
+        for (let i = 0; i < word.length; i++) {
+          let char = word.charAt(i);
+          if (char >= "A" && char <= "Z") {
+            char = char.toLowerCase();
+          } else if (char >= "a" && char <= "z") {
+            char = char.toUpperCase();
+          }
+          newWord += char;
+        }
+
+        return newWord;
+      })
+      .join(" ");
+
+    setText(newText);
+    props.showAlert("Toggled case", "success")
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>
+          {props.heading}
+        </h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}}
+            id="myBox"
+            rows="7"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUC}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLC}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={clearText}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCapitalize}>
+          Capitalize Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={speak}>
+          Speak
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleToggleCaseClick}>
+          Toggle Case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy to Clipboard
+        </button>
+      </div>
+
+      <div className="container my-2" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length - 1} words ,{text.length} characters{" "}
+        </p>
+        <p>
+          {0.008*text.split(" ").length} minutes will be taken to read the words.
+        </p>
+      </div>
+    </>
+  );
+}
